a2f1fd2896f93aa14040d1a5c79efd8d1ffd4c89
rolstu <100982519+rolstu@users.noreply.github.com>
2023-10-07 16:33:24 +0530

    Delete nirvanote_whatsapp_chatbot_server directory

--- nirvanote_whatsapp_chatbot_server/src/routes/Sheet.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {
-  
-  v3updateDbCacheForSheetHandler
-} from "../handlers/Sheet";
-
-export const cronRouter = Router();
-
-cronRouter.post("/v3/fetchDbSheets", v3updateDbCacheForSheetHandler);
--- nirvanote_whatsapp_chatbot_server/src/routes/TrackingSheets.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { addTrackingSheetHandler } from "../handlers/TrackingSheets";
-
-export const router = Router();
-
-router.post("/v1", addTrackingSheetHandler);
